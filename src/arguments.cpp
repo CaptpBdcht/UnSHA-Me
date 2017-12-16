314e92e7fd4ec0366eb62d767be34ce5f5f049be
Mathieu Boisnard <m.boisnard@free.fr>
2017-12-16 17:35:19 +0100

    Remove shit

--- src/arguments.cpp
+++ src/arguments.cpp
@@ -11,11 +11,11 @@ void Arguments::analyseArguments(int argc, char **argv) {
         threadsNumber = static_cast<uint8_t>(std::stoi(argv[1]));
         hashedPassword = argv[2];
     }
-    catch (std::string const& str) {
+    catch (std::string const &str) {
         log.error(std::stringstream() << str);
         exit(EXIT_FAILURE);
     }
-    catch (std::exception const& e) {
+    catch (std::exception const &e) {
         log.error(std::stringstream() << e.what());
         exit(EXIT_FAILURE);
     }
