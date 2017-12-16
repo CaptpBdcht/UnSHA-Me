24a6d82c19f210f72192b0e3cd448855cd608f18
Romuald Ouattara <ouattararomuald@gmail.com>
2017-12-16 14:33:38 +0100

    Clean repository

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
 #include "../include/arguments.hpp"
 #include "../include/threaded-bruteforce.hpp"
-#include "../include/HashProducer.h"
-#include "../include/Consumer.h"
 
 using namespace std;
-
-void generateHashes(HashProducer *);
-
-void startConsuming(Consumer);
 
 void startBruteForce(ThreadedBruteforce *, uint8_t);
 
@@ -20,11 +14,6 @@ int main(int argc, char **argv) {
     Bruteforce *bruteforce = new Bruteforce(arguments.getHashedPassword());
 
     std::vector<std::thread> threads;
-//    std::vector<Bruteforce*> bruteforces;
-//
-//    for (uint8_t i = 1; i <= arguments.getThreadsNumber(); ++i) {
-//        bruteforces.emplace_back(new Bruteforce(arguments.getHashedPassword()));
-//    }
 
     ThreadedBruteforce *tbf = new ThreadedBruteforce(
             arguments.getThreadsNumber(),
@@ -43,42 +32,9 @@ int main(int argc, char **argv) {
     delete bruteforce;
     delete tbf;
 
-//    HashProducer *hashProducer = new HashProducer();
-//    thread producer(generateHashes, hashProducer);
-//
-//    vector<thread> consumers;
-//
-//    for (int i = 0; i < arguments.getThreadsNumber(); ++i) {
-//        consumers.emplace_back(thread(startConsuming,
-//                                      Consumer(arguments.getHashedPassword(), hashProducer)));
-//    }
-//
-//    for (auto &consumer : consumers) {
-//        consumer.join();
-//    }
-//    producer.join();
-//
-//    delete hashProducer;
-
     return EXIT_SUCCESS;
 }
 
 void startBruteForce(ThreadedBruteforce *threadedBruteforce, uint8_t i) {
     threadedBruteforce->threadedBruteforcer(i);
-//    for (uint8_t i = 1; i <= threadedBruteforce->getNbThreads(); ++i) {
-//        threadedBruteforce->threadedBruteforcer(i);
-//        bruteforcers.emplace_front(std::thread(
-//                &ThreadedBruteforce::threadedBruteforcer,
-//                this, i
-//        ));
-//    }
-}
-
-void generateHashes(HashProducer *producer) {
-    producer->startWordGeneration();
-}
-
-void startConsuming(Consumer consumer) {
-    cout << "lanching thread..." << endl;
-    consumer.startConsuming();
 }
